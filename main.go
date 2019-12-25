17b3541d924fb0cd2e612e22d1dede5e610cfa50
Zamir Musaev <zamir.ru@gmail.com>
2019-12-26 00:21:51 +0300

    External secrets storage and working providers

--- main.go
+++ main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"chat/trace"
+	"encoding/json"
 	"flag"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 	"sync"
 	"text/template"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 // templ represents a single template
@@ -16,6 +23,30 @@ type templateHandler struct {
 	once     sync.Once
 	filename string
 	templ    *template.Template
+}
+
+// Secrets is a structure for storing secrets
+// file structure
+// {
+//     "secrets":[
+//         {
+//             "secretName":"chat",
+//             "key":"",
+//             "secret":"",
+//             "url":""
+//         }
+//     ]
+// }
+type Secrets struct {
+	Secrets []Secret `json:"secrets"`
+}
+
+// Secret is a container for storing one secret
+type Secret struct {
+	SecretName string `json:"secretName"`
+	Key        string `json:"key"`
+	Secret     string `json:"secret"`
+	URL        string `json:"url"`
 }
 
 // ServerHttp handles the HTTP request
@@ -29,6 +60,22 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application.")
 	flag.Parse() // Parse the flags
+	// read secrets from a file
+	secrets, err := readSecretsFromJSONFile("secrets.json")
+	if err != nil {
+		log.Fatal("readSecretsFromJSONFile(\"secrets.json\")", err)
+	}
+	secretsMap := make(map[string]Secret)
+	for _, s := range secrets.Secrets {
+		secretsMap[s.SecretName] = s
+	}
+	// setup gominauth
+	gomniauth.SetSecurityKey(secretsMap["chat"].Secret)
+	gomniauth.WithProviders(
+		facebook.New(secretsMap["facebook"].Key, secretsMap["facebook"].Secret, secretsMap["facebook"].URL),
+		github.New(secretsMap["github"].Key, secretsMap["github"].Secret, secretsMap["github"].URL),
+		google.New(secretsMap["google"].Key, secretsMap["google"].Secret, secretsMap["google"].URL),
+	)
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
 	// root
@@ -43,4 +90,23 @@ func main() {
 	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
+}
+
+func readSecretsFromJSONFile(filename string) (*Secrets, error) {
+	jsonFile, err := os.Open(filename)
+	defer jsonFile.Close()
+	if err != nil {
+		return nil, err
+	}
+	byteValue, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return nil, err
+	}
+	var secrets Secrets
+
+	err = json.Unmarshal(byteValue, &secrets)
+	if err != nil {
+		return nil, err
+	}
+	return &secrets, nil
 }
