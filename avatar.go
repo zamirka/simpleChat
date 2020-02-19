572ba7783a4e1d8b5cee526c51166c62611c667b
Zamir Musaev <zamir.ru@gmail.com>
2020-02-20 02:18:04 +0300

    On a halfway to custom avatars

--- avatar.go
+++ avatar.go
@@ -4,9 +4,9 @@ import (
 	"errors"
 )
 
-// ErrNoAvatar is the error that is returned when the
+// ErrNoAvatarURL is the error that is returned when the
 // Avatar instance is unable to provide an avatar URL.
-var ErrNoAvatar = errors.New("chat: Unable to get an avatar URL")
+var ErrNoAvatarURL = errors.New("chat: Unable to get an avatar URL")
 
 // Avatar represents types capable of representing
 // user profile picture
@@ -16,4 +16,25 @@ type Avatar interface {
 	// ErrNoAvatar is returned if the object is unable to get
 	// a URL for the specified client.
 	GetAvatarURL(c *client) (string, error)
+}
+
+// AuthAvatar is implementation of interface above
+type AuthAvatar struct{}
+
+// UseAuthAvatar is an instance of AuthAvatar
+var UseAuthAvatar AuthAvatar
+
+// GetAvatarURL is implementaion of interface method
+func (AuthAvatar) GetAvatarURL(c *client) (string, error) {
+	url, ok := c.userData["avatar_url"]
+	if !ok {
+		return "", ErrNoAvatarURL
+	}
+	urlStr, ok := url.(string)
+	if !ok {
+		return "", ErrNoAvatarURL
+
+	}
+	return urlStr, nil
+
 }
